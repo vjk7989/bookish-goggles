e72d38143c362f8f86eca83f78b690a7c7be434d
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2024-12-21 02:02:55 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mary_Hardy_07-12-24
+Learn how to create a Complete Responsive Personal Portfolio Website from scratch using HTML, CSS, and JavaScript!
---
+++ main.js
@@ -0,0 +1,110 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content h3", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".header__btns", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+
+ScrollReveal().reveal(".intro__image", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+ScrollReveal().reveal(".intro__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".intro__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".intro__description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".intro__grid", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".intro__content h4", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+ScrollReveal().reveal(".intro__flex div", {
+  ...scrollRevealOption,
+  delay: 3000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".journey__grid > div > div", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const mixer = mixitup(".portfolio__grid");
+
+ScrollReveal().reveal(".banner__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".banner__content p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".banner__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  spaceBetween: 50,
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+ScrollReveal().reveal(".blog__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
